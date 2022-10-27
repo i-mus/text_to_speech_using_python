431b58a46cd8b3dddca10244a2a9364faf40e060
i-mus <mushthaqpullat@gmail.com>
2022-10-27 17:38:50 +0530

    Developed file to audio

---
+++ file_to_aud.py
@@ -0,0 +1,8 @@
+from gtts import gTTS
+import os
+# converting file data to audio
+sec_text = open("demo.txt", "r").read()
+language = "en"
+sec_output = gTTS(text=sec_text, lang=language, slow=False)
+sec_output.save("fileout.mp3")
+os.system("start fileout.mp3")
